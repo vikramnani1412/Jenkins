3dcc66e4ac191e401d494c3d530ee0060d99f4c5
Vicky1412 <126240447+vikram141234@users.noreply.github.com>
2024-06-25 18:27:53 +0530

    Commit

---
+++ src/test/java/SamplePrinting/One.java
@@ -0,0 +1,15 @@
+package SamplePrinting;
+
+public class One {
+
+	public static void main(String[] args) {
+		System.out.println("Jenkins Check 1");
+		One o = new One();
+		o.printing();
+	}
+	public void printing()
+	{
+		System.out.println("Jenkins Check 2");
+	}
+	
+}
